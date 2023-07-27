372755776c1d3046c67cb14fe9531aa966c38a2b
Godslove Lee <godslovelee@yahoo.com>
2023-07-27 22:01:47 +0200

    Add AppConfig file

---
+++ lib/routes/food_pickup_routes.dart
@@ -0,0 +1,22 @@
+
+import '../screens/home_screen.dart';
+import 'app_routes_config.dart';
+
+class FoodPickup extends AppRoutesSettings{
+  @override
+  // TODO: implement home
+  String get home => HomePage.route;
+
+  @override
+  // TODO: implement lockScreen
+  String get lockScreen => throw UnimplementedError();
+
+  @override
+  // TODO: implement pickupBoxes
+  String get pickupBoxes => throw UnimplementedError();
+
+  @override
+  // TODO: implement splashScreen
+  String get splashScreen => throw UnimplementedError();
+
+}
